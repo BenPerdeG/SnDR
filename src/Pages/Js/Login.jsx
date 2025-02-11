053353acadddd68e6d15b0d2af8055a93784fff4
dam2a <bperez@elpuig.xeill.net>
2025-02-11 16:33:56 +0100

    petit cambios

--- src/Pages/Js/Login.jsx
+++ src/Pages/Js/Login.jsx
@@ -10,10 +10,10 @@ const Login = () => {
       <header className="LoginHeader">
         <h2 className="logo">Logo</h2>
         <nav className="navigation">
-          <a href="#">Inicio</a>
-          <a href="#">Mis Partidas</a>
-          <a href="#">Buscar Partidas</a>
-          <a href="#">Mi Perfil</a>
+          <a href="inicio">Inicio</a>
+          <a href="misPartidas">Mis Partidas</a>
+          <a href="search">Buscar Partidas</a>
+          <a href="profile">Mi Perfil</a>
           <button className="btnLogin-popup" onClick={() => setIsPopUp(true)}>Login</button>
         </nav>
       </header>
