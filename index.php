bd7a5865701a72e5693dc9e84af36047b630981d
dam2a <bperez@elpuig.xeill.net>
2025-02-12 20:26:08 +0100

    .

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
-<?php
-$conn=new mysqli("sql111.infinityfree.com ","if0_38158122","lO0WGtEGoOc4T0"," if0_38158122_sndr ");
-$sql = "INSERT INTO Usuario(nombre,email,contraseña) VALUES (a,b@b,c);";
-if($conn->query($sql))
-{
-    echo "value inserted";
-}
-else{
-    echo "insertion failed";
-}
-$conn->close();
-?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="./src/assets/logo.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Stats & DiceRolls</title>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script type="module" src="/src/main.jsx"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+      
+  </body>
+  </html>
+
